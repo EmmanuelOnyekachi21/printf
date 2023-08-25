2742e4d4bff667c84cd962cc907143783a748986
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-25 09:23:32 -0700

    Updated _putstr.c

--- _putstr.c
+++ _putstr.c
@@ -8,9 +8,9 @@
  *
  * Return: number of printed charcters.
  */
-int _putstr(va_list args)
+int _putstr(va_list str)
 {
-	char *ptr = va_arg(args, char *);
+	char *ptr = va_arg(str, char *);
 	int count = 0;
 
 	/* Check for NULL */
