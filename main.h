251d7809462a8e6aed011b3bcd58638d0c3c1a7a
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-25 02:14:45 -0700

    Updated header

--- main.h
+++ main.h
@@ -2,15 +2,15 @@
 #define _MAIN_H_
 #include <stdarg.h>
 /**
- * struct structure - structure holding a char and a function pointer.
+ * struct structures - structure holding a char and a function pointer.
  * @letter: format specifier charcater
  * @fxnpoint: Function pointr to handle specifier
  */
-struct structure
+typedef struct structures
 {
 	char letter;
 	int (*fxnpoint)(va_list args);
-};
+} arrstr;
 int _putchar(char c);
 int print_char(va_list args);
 int _printf(const char *format, ...);
